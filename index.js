05bdbfff43bcaca4355c593cc778bcc3635c7569
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-03-20 21:15:48 +0530

    sum(need to be updated)
    nested loop and all to be updated.

---
+++ index.js
@@ -0,0 +1,16 @@
+// run the code seperately because i has been used frequently
+// for loop
+// let i;
+// let j = "Praveen raj K"
+// for (i = 0; i < j.length; i++) {
+//     console.log(`${j[i] } ${i}`);
+// }
+// nested for loop break and continue
+let i;
+let j;
+for(i=1;i<=10;i++){
+    for(j=1;j<=i;j++){
+        console.log(`i ${i} j ${j}`);
+    }
+}
+
