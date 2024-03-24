081b8f0150daaa46735f0782fdd5495fb6f4cf06
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-03-24 21:05:35 +0530

    while and dowhile updated
    Desc(while and dowhile updated)

---
+++ doWhile.js
@@ -0,0 +1,11 @@
+// doWhile
+let i = 1;
+
+do {
+    if (i==5) {
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+} while (i<=10);
---
+++ while.js
@@ -0,0 +1,12 @@
+// while loop;
+// not clear verify again;
+// how does break and continue works? in while loops;
+let i = 1;
+while (i <= 10) {
+    if (i==5) {
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+}
