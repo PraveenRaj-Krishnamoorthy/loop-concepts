d84e4d606220357fa89e88e9d2c86fb1e5411ca0
PraveenRaj-Krishnamoorthy <praveenraj.krishnamoorthy@outlook.com>
2024-03-25 08:35:19 +0530

    while and do while updated
    wwhile and doWhile updated!

--- doWhile.js
+++ doWhile.js
@@ -1,11 +1,20 @@
 // doWhile
-let i = 1;
+// let i = 1;
 
-do {
-    if (i==5) {
-        i++;
-        continue;
-    }
-    console.log(i);
-    i++;
-} while (i<=10);
+// do {
+//     if (i==5) {
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// } while (i<=10);
+
+// -----------------------------------------//
+
+// let i = 1;
+// let j = "Praveen Raj K"
+// do {
+// console.log(`i ${i} j ${j}`);
+//     i++;
+// } while (i <=10)
--- while.js
+++ while.js
@@ -1,12 +1,27 @@
 // while loop;
 // not clear verify again;
 // how does break and continue works? in while loops;
-let i = 1;
-while (i <= 10) {
-    if (i==5) {
-        i++;
-        continue;
-    }
-    console.log(i);
-    i++;
-}
+// let i = 1;
+// while (i <= 10) {
+//     if (i==5) {
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// --------------------------------------------------//
+
+
+
+
+// loop starts from 0 until i<j !<=
+// let i=0;
+// let j = "Praveen Raj K"
+// while(i<j.length){
+//     console.log(`i ${i} & ${j} & ${j[i]}`);
+//     i++;
+// }
